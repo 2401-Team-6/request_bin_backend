5b4d6cce3c5750c5370aac01824f305be6c488fd
Christopher Perkins <Christopher.Perkins@null.net>
2024-01-29 16:27:56 -0500

    Parses all bodies as text

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.all('/', (req, res, next) => {
-  console.log(req);
+app.use('/log', express.text({ type: '*/*' }));
+
+app.all('/log', (req, res, next) => {
+  console.log(req.path, req.method, req.headers, req.body);
   res.status(200);
 });
 
