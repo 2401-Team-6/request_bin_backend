b3b5118c8a80560fc8ed69f2d61bb32c589cc89e
Vincent Sijssens <vincentsijssens@Vincents-MacBook-Pro.local>
2024-01-30 17:56:12 -0500

    dotenv

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+require ("dotenv").config();
+
 const app = express();
 const port = 3000;
 
 const logger = require('./src/db/psql_connector');
 const mongo = require('./src/db/mongo_connector');
 
-const nanoid = require('nanoid');
+const { nanoid } = require('nanoid');
 
 app.use('/log', express.text({ type: '*/*' }));
 
