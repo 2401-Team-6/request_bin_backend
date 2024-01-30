e4dc51ff1c0775384095c2f6240f6dd5d4b5029c
Vincent Sijssens <vincentsijssens@Vincents-MacBook-Pro.local>
2024-01-30 17:33:20 -0500

    added endpoint generator and route

--- index.js
+++ index.js
@@ -5,14 +5,23 @@ const port = 3000;
 const logger = require('./src/db/psql_connector');
 const mongo = require('./src/db/mongo_connector');
 
+const nanoid = require('nanoid');
+
 app.use('/log', express.text({ type: '*/*' }));
 
 app.all('/log', async (req, res, next) => {
   // logger.createRequest(1, req.method, req.url, 'Blah');
   // console.log(logger.getRequest(0));
-  id = await mongo.createRequest(req.headers, req.body);
+  const id = await mongo.createRequest(req.headers, req.body);
   console.log(id);
   res.status(200);
+});
+
+app.post('/api/new', async (req, res, next) => {
+  const endpoint = nanoid();
+  await logger.createEndpoint(endpoint);
+  console.log(endpoint);
+  res.json({ endpoint });
 });
 
 app.listen(port, () => {
--- src/db/psql_connector.js
+++ src/db/psql_connector.js
@@ -1,3 +1,6 @@
+const { Client } = require('pg');
+const client = new Client();
+
 const request = [];
 
 function createRequest(mongoId, method, path, endpointId) {
@@ -10,8 +13,14 @@ function createRequest(mongoId, method, path, endpointId) {
   });
 }
 
+async function createEndpoint(name) {
+  await client.connect();
+  client.query('INSERT INTO endpoints (name) VALUES ($1) RETURNING *', [name]);
+  await client.end();
+}
+
 function getRequest(id) {
   return request[id];
 }
 
-module.exports = { createRequest, getRequest };
+module.exports = { createRequest, getRequest , createEndpoint};
