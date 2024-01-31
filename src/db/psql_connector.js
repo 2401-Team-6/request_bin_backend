55b060495dad70082061b4b57dc44df8a5b3ac45
Christopher Perkins <Christopher.Perkins@null.net>
2024-01-31 18:04:30 -0500

    Removed logging from psql_connector

--- src/db/psql_connector.js
+++ src/db/psql_connector.js
@@ -5,7 +5,10 @@ async function createRequest(mongoId, method, path, endpointId) {
   const client = new Client();
 
   await client.connect();
-  await client.query('INSERT INTO requests (mongo_id, method, path, endpoint_id) VALUES ($1, $2, $3, $4)', [mongoId, method, path, endpointId]);
+  await client.query(
+    'INSERT INTO requests (mongo_id, method, path, endpoint_id) VALUES ($1, $2, $3, $4)',
+    [mongoId, method, path, endpointId]
+  );
   await client.end();
 }
 
@@ -13,7 +16,10 @@ async function getEndpointId(name) {
   const client = new Client();
 
   await client.connect();
-  const response = await client.query('SELECT id FROM endpoints WHERE name = $1', [name]);
+  const response = await client.query(
+    'SELECT id FROM endpoints WHERE name = $1',
+    [name]
+  );
   await client.end();
   return response.rows[0]?.id;
 }
@@ -22,9 +28,10 @@ async function createEndpoint(name) {
   const client = new Client();
 
   await client.connect();
-  const endpoint = await client.query('INSERT INTO endpoints (name) VALUES ($1) RETURNING *', [name]);
-  console.log(endpoint.rows);
-  // send back id and hash
+  const endpoint = await client.query(
+    'INSERT INTO endpoints (name) VALUES ($1) RETURNING *',
+    [name]
+  );
   await client.end();
   return endpoint.rows[0];
 }
@@ -33,15 +40,17 @@ async function getRequests(id) {
   const client = new Client();
 
   await client.connect();
-  const response = await client.query('SELECT mongo_id, method, path, created FROM requests WHERE endpoint_id = $1', [id]);
-  const requests = response.rows.map(row => {
-    // add timestamp
-    return ({
+  const response = await client.query(
+    'SELECT mongo_id, method, path, created FROM requests WHERE endpoint_id = $1',
+    [id]
+  );
+  const requests = response.rows.map((row) => {
+    return {
       id: row.mongo_id,
       method: row.method,
       path: row.path,
       created: row.created,
-    });
+    };
   });
   return requests;
 }
