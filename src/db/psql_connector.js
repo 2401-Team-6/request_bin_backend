c065bc27dc7d82ce9d4b3fb6b06baa0458cf9547
Christopher Perkins <Christopher.Perkins@null.net>
2024-01-31 22:54:07 -0500

    Changed psql_connector to use Pool instead of Client

--- src/db/psql_connector.js
+++ src/db/psql_connector.js
@@ -1,58 +1,40 @@
-const { Client } = require('pg');
-const request = [];
+const { Pool } = require('pg');
+
+const client = new Pool();
 
 async function createRequest(mongoId, method, path, endpointId) {
-  const client = new Client();
-
-  await client.connect();
   response = await client.query(
     'INSERT INTO requests (mongo_id, method, path, endpoint_id) VALUES ($1, $2, $3, $4) RETURNING created',
     [mongoId, method, path, endpointId]
   );
-  await client.end();
   return response.rows[0].created;
 }
 
 async function getEndpointId(name) {
-  const client = new Client();
-
-  await client.connect();
   const response = await client.query(
     'SELECT id FROM endpoints WHERE name = $1',
     [name]
   );
-  await client.end();
   return response.rows[0]?.id;
 }
 
 async function getEndpoint(name) {
-  const client = new Client();
-
-  await client.connect();
   const response = await client.query(
     'SELECT * FROM endpoints WHERE name = $1',
     [name]
   );
-  await client.end();
   return response.rows[0];
 }
 
 async function createEndpoint(name) {
-  const client = new Client();
-
-  await client.connect();
   const endpoint = await client.query(
     'INSERT INTO endpoints (name) VALUES ($1) RETURNING *',
     [name]
   );
-  await client.end();
   return endpoint.rows[0];
 }
 
 async function getRequests(id) {
-  const client = new Client();
-
-  await client.connect();
   const response = await client.query(
     'SELECT mongo_id, method, path, created FROM requests WHERE endpoint_id = $1',
     [id]
@@ -69,19 +51,11 @@ async function getRequests(id) {
 }
 
 async function deleteRequest(id) {
-  const client = new Client();
-
-  await client.connect();
   await client.query('DELETE FROM requests WHERE mongo_id = $1', [id]);
-  await client.end();
 }
 
 async function deleteAll(id) {
-  const client = new Client();
-
-  await client.connect();
   await client.query('DELETE FROM requests WHERE endpoint_id = $1', [id]);
-  await client.end();
 }
 
 module.exports = {
