ed87329cbeaa3008d5b086e4c63a6a978d441a57
Morgan Davis <memlin@Morgans-Surface.>
2024-02-01 07:59:02 -0500

    Changed db schema to store timestamps in epoch time

--- db/requestTable.sql
+++ db/requestTable.sql
@@ -1,14 +1,17 @@
+DROP TABLE requests;
+DROP TABLE endpoints;
+
 CREATE TABLE endpoints (
   id serial PRIMARY KEY,
   name char(21) NOT NULL UNIQUE,
-  created TIMESTAMP NOT NULL DEFAULT(CURRENT_TIMESTAMP)
+  created numeric NOT NULL DEFAULT(EXTRACT(EPOCH FROM CURRENT_TIMESTAMP))
 );
 
 CREATE TABLE requests (
   id serial PRIMARY KEY,
   mongo_id char(24) NOT NULL UNIQUE,
   endpoint_id integer REFERENCES endpoints(id) NOT NULL,
-  created TIMESTAMP NOT NULL DEFAULT(CURRENT_TIMESTAMP),
+  created numeric NOT NULL DEFAULT(EXTRACT(EPOCH FROM CURRENT_TIMESTAMP)),
   method varchar(10) NOT NULL,
   path varchar(50) NOT NULL
-)
+);
